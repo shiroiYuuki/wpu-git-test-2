a695b74cf6e821c1b10dcc4a0a6f3c6061249909
yuukianakmagang <yuukianakmagang@gmail.com>
2025-08-09 14:42:30 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
 </head>
 <body>
     <h1>HElLO WORLD</h1>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo enim tenetur repudiandae cum nam nesciunt a fuga autem vel earum? Soluta autem eaque, alias praesentium voluptatibus obcaecati excepturi inventore laboriosam!</p>
 </body>
 </html>
